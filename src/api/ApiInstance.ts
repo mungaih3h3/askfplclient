068af86d6c59ec3ac915f6c6a5b0efae17c6da22
Mungai <itsmungai@gmail.com>
2021-10-19 07:23:26 +0300

    modified env url

--- src/api/ApiInstance.ts
+++ src/api/ApiInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const url =
   process.env.NODE_ENV === "production"
-    ? "http://brianmungai.com"
+    ? "https://www.brianmungai.com"
     : "http://localhost:5050";
 
 export const apiInstance = axios.create({
