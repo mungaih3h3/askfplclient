6e0940422ffdae8e6a28622b80a66c6045d9edd6
Mungai <itsmungai@gmail.com>
2021-10-27 05:20:57 +0300

    layout for desktop

---
+++ src/components/present/CUser.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react";
+import User from "../../logic/User";
+
+interface CUserProps {
+    user: User
+}
+ 
+const CUser: FC<CUserProps> = ({user}) => {
+    return (  );
+}
+ 
+export default CUser;
