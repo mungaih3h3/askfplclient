c88e6995675b165ef06a9abfeefddcc716cb5615
Mungai <itsmungai@gmail.com>
2021-11-08 10:56:06 +0300

    ip no certificate

--- src/api/ApiInstance.ts
+++ src/api/ApiInstance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const url =
   process.env.NODE_ENV === "production"
-    ? "https://70.39.249.233"
+    ? "https://brianmungai.com"
     : "http://192.168.0.108:5050";
 
 export const apiInstance = axios.create({
